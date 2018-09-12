0fb80df08659357c7afdfee3a8ee791cd9120139
Paolo 'VoidSec' Stagno <voidsec@voidsec.com>
2018-09-12 13:13:30 +0200

    Fixed Small syntax error #1

--- Introspection/GraphQL_Introspection.py
+++ Introspection/GraphQL_Introspection.py
@@ -56,7 +56,7 @@ def query(target,key):
 	if key:
 		headers = {
 			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0"
-			"Authorization: "+key
+			"Authorization: " key
 		}
 	else:
 		headers = {
