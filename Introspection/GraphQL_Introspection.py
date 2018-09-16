6d946f86e2fe0ba2812850c53757eac3f0fcf130
alex-birsan <alex-birsan@users.noreply.github.com>
2018-09-16 21:29:10 +0300

    Fix syntax error

--- Introspection/GraphQL_Introspection.py
+++ Introspection/GraphQL_Introspection.py
@@ -55,7 +55,7 @@ def query(target,key):
 	#-----------------------
 	if key:
 		headers = {
-			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0"
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0",
 			"Authorization": key
 		}
 	else:
