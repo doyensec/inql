a2dcc78e201fbcaef9367b21e6fb662353cd9ea9
VoidSec <voidsec@voidsec.com>
2018-12-26 15:00:51 +0100

    minor fixes
    inline output instead of big nested bullet list

--- Introspection/GraphQL_Introspection.py
+++ Introspection/GraphQL_Introspection.py
@@ -9,48 +9,93 @@ import requests
 import argparse
 import sys
 import time
+import os
+from urlparse import urlparse
+from datetime import date
 
-RED="\033[1;31;10m"
-GREEN="\033[1;32;10m"
-YELLOW="\033[1;37;10m"
-WHITE="\033[1;37;10m"
+RED = "\033[1;31;10m"
+GREEN = "\033[1;32;10m"
+YELLOW = "\033[1;37;10m"
+WHITE = "\033[1;37;10m"
 stl = """
 <style>
- body {
+body {
   font-family: Roboto;
   background-color: #f9f9f9;
- }
- li.query {
+}
+
+li.query {
   color: #368cbf;
- }
- li.mutation {
+}
+
+li.mutation {
   color: #30a;
- }
- li.subscription {
+}
+
+li.subscription {
   color: #397D13;
- }
- li.argument {
+}
+
+li.argument {
   color: #edae49;
- }
+}
+
 li.type {
-  olor: #7ebc59;
- }
- li.deprecated {
+  color: #7ebc59;
+}
+
+li.deprecated {
   color: red;
   ext-decoration: underline wavy red;
- }
- li.field {
- }
- li.description {
+}
+
+li.field {
+
+}
+
+li.description {
   color: grey;
- }
- div.box {
+}
+span.query {
+  color: #368cbf;
+}
+
+span.mutation {
+  color: #30a;
+}
+
+span.subscription {
+  color: #397D13;
+}
+
+span.argument {
+  color: #edae49;
+}
+
+span.type {
+  color: #7ebc59;
+}
+
+span.deprecated {
+  color: red;
+  ext-decoration: underline wavy red;
+}
+
+span.field {
+
+}
+
+span.description {
+  color: grey;
+}
+
+div.box {
   background-color: white;
   width: 300px;
   border: 5px solid grey;
   padding: 10px;
   margin: 10px;
- }
+}
 </style>
 """
 
@@ -65,6 +110,7 @@ def query(target, key):
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0",
             "Authorization": key
+            # TODO add the option for custom headers
         }
     else:
         headers = {
@@ -80,33 +126,42 @@ def query(target, key):
             if request.status_code == 200:
                 return request
             else:
-                print "Query failed! Code {}".format(request.status_code)
+                print RED + "Query failed! Code {}".format(request.status_code) + WHITE
                 sys.exit(1)
     except requests.exceptions.MissingSchema:
-        print RED+"Missing http:// or https:// schema from Target URL"+WHITE
+        print RED + "Missing http:// or https:// schema from Target URL" + WHITE
         sys.exit(1)
     except requests.exceptions.ConnectionError:
-        print RED+"Failed to establish a new connection: Connection refused"+WHITE
+        print RED + "Failed to establish a new connection: Connection refused" + WHITE
         sys.exit(1)
+
+
+def check_dir(file_path):
+    directory = os.path.dirname(file_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
 
 
 def main():
-    # TODO: should be interesting to generate SDL file
     parser = argparse.ArgumentParser(prog="GraphQL_Introspection.py", description="GraphQL Introspection")
     parser.add_argument("-t", required=True, dest="target",
                         help="Remote GraphQL Endpoint (https://<Target_IP>/graphql)")
     parser.add_argument("-k", dest="key", help="API Authentication Key")
-    parser.add_argument("-o", "--output", required=True, help="Directs the output to a file of your choice")
     parser.add_argument("-c", dest="custom", action='store_true', default=False,
                         help="Add custom objects to the output (verbose)")
     args = parser.parse_args()
 
-    if args.target and args.output:
-        timestamp = time.time()
-        with open("doc-" + args.output + str(int(timestamp)) + ".html", 'w') as output_file:
+    if args.target:
+        URL = urlparse(args.target).netloc
+        timestamp = time.time()  # str(int(timestamp))
+        today = str(date.today())
+        # Create directory structure
+        check_dir(URL + "/")
+
+        with open(URL + "/doc-" + today + ".html", 'w') as output_file:
             result = query(args.target, args.key)
             # Printing schema file
-            schema_file = open("schema-" + str(int(timestamp)) + ".txt", "w")
+            schema_file = open(URL + "/schema-" + today + ".txt", "w")
             schema_file.write(result.text)  # return clean json
             schema_file.close()
             # returns a dict
@@ -138,9 +193,8 @@ def main():
                         "<li class='subscription'>{0}</li></ul>\n".format(
                             result['data']['__schema']['subscriptionType']['name']))
                     Subscription = result['data']['__schema']['subscriptionType']['name']
-
+                ##########################################################################################
                 i = 0
-                output_file.write("<br>")
                 # data
                 #   __schema
                 #       directives
@@ -149,10 +203,8 @@ def main():
                 #       subscriptionType
                 #       types
                 # Start looping trough types
-                # TODO: generate documentation in-line instead of using bullets
                 if result['data']['__schema']['types'] is not None:
                     rt = result['data']['__schema']['types']
-                    output_file.write("<ul>\n")
                     for types in rt:
                         j = 0
                         # Data -> Schema -> Types (kind, name, description)
@@ -178,8 +230,8 @@ def main():
                             else:
                                 if rt[i]['description'] is not None:
                                     output_file.write(
-                                        "<li class='description'>{0}</li>\n".format(rt[i]['description']))
-                                output_file.write("<li class='type'>{0}</li>\n".format(rt[i]['name']))
+                                        "<span class='description'>{0}</span>\n".format(rt[i]['description']))
+                                output_file.write("<span class='type'>{0}</span>\n".format(rt[i]['name']))
                         k = 0
                         # Data -> Schema -> Types -> enumValues (name, description, isDeprecated, deprecationReason)
                         # TODO retieve enum values where present
@@ -191,21 +243,19 @@ def main():
                                         rt[i]['name'] == Subscription))))) or (
                                 custom is not False and ((rt[i]['enumValues'] is not None) and (
                                 rt[i]['name'] not in primitives) and (rt[i]['kind'] not in advanced_kind)))):
-                            output_file.write("<ul>\n")
                             for enumValues in rt[i]['enumValues']:
                                 if rt[i]['enumValues'][k]['name'] is not None:
-                                    output_file.write("<li>{0}</li>\n".format(rt[i]['enumValues'][k]['name']))
+                                    output_file.write("<span>{0}</span>\n".format(rt[i]['enumValues'][k]['name']))
                                 if rt[i]['enumValues'][k]['description'] is not None:
-                                    output_file.write("<ul><li class='description'>{0}</li></ul>\n".format(
+                                    output_file.write("<span class='description'>{0}</span>\n".format(
                                         rt[i]['enumValues'][k]['description']))
                                 if rt[i]['enumValues'][k]['isDeprecated'] is not False and rt[i]['enumValues'][k][
                                     'isDeprecated'] is not None:
-                                    output_file.write("<ul><li class='deprecated'>Is Deprecated</li></ul>\n")
+                                    output_file.write("<span class='deprecated'>Is Deprecated</span>\n")
                                 if rt[i]['enumValues'][k]['deprecationReason'] is not None:
-                                    output_file.write("<ul><li>Reason: {0}</li></ul>\n".format(
+                                    output_file.write("<span>Reason: {0}</span>\n".format(
                                         rt[i]['enumValues'][k]['deprecationReason']))
                                 k = k + 1
-                            output_file.write("</ul><br>")
                         # Data -> Schema -> Types -> Fields (name, isDeprecated, deprecationReason, description)
                         if ((custom is False and ((
                                                           rt[i]['fields'] is not None) and (
@@ -218,7 +268,6 @@ def main():
                                                                  rt[i]['fields'] is not None) and (
                                                                  rt[i]['name'] not in primitives) and (
                                                                  rt[i]['kind'] not in advanced_kind)))):
-                            output_file.write("<ul>")
                             for fields in result['data']['__schema']['types'][i]['fields']:
                                 if rt[i]['fields'][j]['name'] is not None:
                                     if rt[i]['name'] == Query:
@@ -237,30 +286,31 @@ def main():
                                         output_file.write("<li>{0}</li>\n".format(rt[i]['fields'][j]['name']))
                                 if rt[i]['fields'][j]['isDeprecated'] is not False and rt[i]['fields'][j][
                                     'isDeprecated'] is not None:
-                                    output_file.write("<li class='deprecated'>Is Deprecated</li>\n")
+                                    output_file.write("<span class='deprecated'>Is Deprecated</span>\n")
                                 if rt[i]['fields'][j]['deprecationReason'] is not None:
                                     output_file.write(
-                                        "<li>Reason: {0}</li>\n".format(rt[i]['fields'][j]['deprecationReason']))
+                                        "<span>Reason: {0}</span>\n".format(rt[i]['fields'][j]['deprecationReason']))
                                 if rt[i]['fields'][j]['description'] is not None and rt[i]['fields'][j][
                                     'description'] != '':
                                     output_file.write(
-                                        "<li class='description'>{0}</li>\n".format(rt[i]['fields'][j]['description']))
+                                        "<span class='description'>{0}</span>\n".format(
+                                            rt[i]['fields'][j]['description']))
                                 if rt[i]['fields'][j]['type'] is not None:
                                     if rt[i]['fields'][j]['type']['name'] is not None:
-                                        output_file.write("<ul><li class='type'>{0}</li></ul>\n".format(
+                                        output_file.write("<span class='type'>{0}</span>\n".format(
                                             rt[i]['fields'][j]['type']['name']))
                                 if rt[i]['fields'][j]['type']['ofType'] is not None and \
                                         rt[i]['fields'][j]['type']['ofType']['name'] is not None:
                                     if rt[i]['fields'][j]['type']['kind'] is not None and rt[i]['fields'][j]['type'][
                                         'kind'] == "LIST":
-                                        output_file.write("<ul><li class='type'>[{0}]</li></ul>\n".format(
+                                        output_file.write("<span class='type'>[{0}]</span>\n".format(
                                             rt[i]['fields'][j]['type']['ofType']['name']))
                                     elif rt[i]['fields'][j]['type']['kind'] is not None and rt[i]['fields'][j]['type'][
                                         'kind'] == "NON_NULL":
-                                        output_file.write("<ul><li class='type'>!{0}</li></ul>\n".format(
+                                        output_file.write("<span class='type'>!{0}</span>\n".format(
                                             rt[i]['fields'][j]['type']['ofType']['name']))
                                     else:
-                                        output_file.write("<ul><li class='type'>{0}</li></ul>\n".format(
+                                        output_file.write("<span class='type'>{0}</span>\n".format(
                                             rt[i]['fields'][j]['type']['ofType']['name']))
                                 x = 0
                                 if ((custom is False and ((rt[i]['fields'][j]['args'] is not None) and (
@@ -272,50 +322,45 @@ def main():
                                                                           rt[i]['name'] == Subscription))))) or (
                                         custom is not False and ((rt[i]['fields'][j]['args'] is not None) and (
                                         rt[i]['name'] not in primitives) and (rt[i]['kind'] not in advanced_kind)))):
-                                    output_file.write("<ul>\n")
                                     # Data -> Schema -> Types -> Fields -> Args (defaultValue, name, description)
                                     for args in rt[i]['fields'][j]['args']:
                                         if rt[i]['fields'][j]['args'][x]['defaultValue'] is not None:
                                             output_file.write(
-                                                "<li>{0}</li>\n".format(rt[i]['fields'][j]['args'][x]['defaultValue']))
+                                                "<span>{0}</span>\n".format(
+                                                    rt[i]['fields'][j]['args'][x]['defaultValue']))
                                         if rt[i]['fields'][j]['args'][x]['name'] is not None:
-                                            output_file.write("<li class='argument'>{0}</li>\n".format(
+                                            output_file.write("<span class='argument'>{0}</span>\n".format(
                                                 rt[i]['fields'][j]['args'][x]['name']))
                                         if rt[i]['fields'][j]['args'][x]['description'] is not None and \
                                                 rt[i]['fields'][j]['args'][x]['description'] != '':
-                                            output_file.write("<li class='description'>{0}</li>\n".format(
+                                            output_file.write("<span class='description'>{0}</span>\n".format(
                                                 rt[i]['fields'][j]['args'][x]['description']))
                                         # Data -> Schema -> Types -> Fields -> Args -> Type (name, ofType, kind)
                                         if rt[i]['fields'][j]['args'][x]['type'] is not None and (
                                                 rt[i]['name'] not in primitives) and (
                                                 rt[i]['kind'] not in advanced_kind):
-                                            output_file.write("<ul>")
                                             if rt[i]['fields'][j]['args'][x]['type']['kind'] == "LIST":
-                                                output_file.write("<li class='type'>[{0}]</li>\n".format(
+                                                output_file.write("<span class='type'>[{0}]</span>\n".format(
                                                     rt[i]['fields'][j]['args'][x]['type']['ofType']['name']))
                                             elif rt[i]['fields'][j]['args'][x]['type']['kind'] == "NON_NULL":
-                                                output_file.write("<li class='type'>!{0}</li>\n".format(
+                                                output_file.write("<span class='type'>!{0}</span>\n".format(
                                                     rt[i]['fields'][j]['args'][x]['type']['ofType']['name']))
                                             else:
                                                 if rt[i]['fields'][j]['args'][x]['type']['name'] is not None:
-                                                    output_file.write("<li class='type'>{0}</li>\n".format(
+                                                    output_file.write("<span class='type'>{0}</span>\n".format(
                                                         rt[i]['fields'][j]['args'][x]['type']['name']))
                                                 if rt[i]['fields'][j]['args'][x]['type']['ofType'] is not None:
-                                                    output_file.write("<li>{0}</li>\n".format(
+                                                    output_file.write("<span>{0}</span>\n".format(
                                                         rt[i]['fields'][j]['args'][x]['type']['ofType']))
-                                            output_file.write("</ul>\n")
                                         x = x + 1
-                                    output_file.write("</ul><br>\n")
                                 j = j + 1
-                            output_file.write("</ul><br>\n")
                         i = i + 1
-                    output_file.write("</ul>\n")
             # except KeyError:
             except Exception:
                 raise
             output_file.write("</body></html>")
             output_file.close()
-            print GREEN+"[DONE]"+WHITE
+            print GREEN + "[DONE]" + WHITE
             sys.exit(0)
     else:
         print "Missing Arguments"
@@ -326,5 +371,5 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print RED+"Exiting..."+WHITE
+        print RED + "Exiting..." + WHITE
         sys.exit(1)
