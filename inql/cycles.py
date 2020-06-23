cc7440040b8df56ae7864004756ca0e4ca91b935
Andrea Brancaleoni <abc@pompel.me>
2020-06-23 21:26:45 +0200

    inql.cycles: first working cycles detector
    first working cycles detector for inql.
    Still no ending case is created for weird cases.
    Remember to run it timeboxed.

--- inql/cycles.py
+++ inql/cycles.py
@@ -25,7 +25,10 @@ class Graph:
                     if target == 'union':
                         type_name = fields
                     else:
+                        if fields[0:2] == "__":
+                            continue
                         type_name = object_type[fields]['type']
+
                     if type_name not in self.graphQLTypes:
                         if target == 'union':
                             tmp_reference_list.append({
@@ -52,29 +55,6 @@ class Graph:
                 }
 
     def _connect(self):
-        """
-          function connectVertices() {
-    for ( var vertex in Graph) {
-      for ( var ref in Graph[vertex].referenceList) {
-        var reference = Graph[vertex].referenceList[ref].reference;
-        edges++;
-        if(Graph[reference] === undefined)
-        {
-          if(excludeList.includes(reference))
-          {
-            Graph[reference] = {"vertexID": reference, "referenceList":[]};
-          } // added to avoid when someone refers to the subscription|query|mutation types
-          else
-          {
-            throw new Error("Field " + reference +  " not defined");
-          }
-        }
-        Graph[vertex].referenceList[ref].reference = Graph[reference];
-      }
-    }
-  }
-
-        """
         for vertex in self._representation.keys():
             for ref, ref_object in enumerate(self._representation[vertex]['referenceList']):
                 reference = ref_object['reference']
@@ -85,7 +65,6 @@ class Graph:
                     else:
                         raise Exception("Field %s not defined" % reference)
                 ref_object['reference'] = self._representation[reference]
-
 
     def __repr__(self):
         return str({
@@ -121,3 +100,206 @@ class Graph:
         self._created = True
 
         return self._representation
+
+class CyclesDetector:
+    def __init__(self, graph):
+        self.graph = graph
+        self.save_mode = 1
+        self.data = None
+
+    def detect(self, select=False):
+        if self.data:
+            return self.data
+
+        data = {}
+        data['foundCycle'] = False
+        data['cycles'] = []
+        data['numberCycles'] = 0
+        tarjan = TarjanAlgorithm(self.graph, select)
+        tarjan.execute()
+
+        data['SCCs'] = len(tarjan.scc)
+        data['longestSCC'] = 0
+
+        for component, _ in enumerate(tarjan.scc):
+            if len(tarjan.scc[component]) > data['longestSCC']:
+                data['longestSCC'] = len(tarjan.scc[component])
+
+        if select:
+            data['foundCycle'] = tarjan.found_cycle
+            return data
+
+        tarjan.prune_edges()
+
+        for i, _ in enumerate(tarjan.scc):
+            try:
+                johnson = JohnsonAlgorithm(tarjan.scc[i])
+                johnson.execute()
+                data['cycles'] += johnson.cycles
+                data['numberCycles'] += len(johnson.cycles)
+            except Exception as ex:
+                print(ex)
+                print("#Cycles found = %s" % len(johnson.cycles))
+                data['numberCycles'] += len(johnson.cycles)
+                data['cycles'] += johnson.cycles
+                return data
+
+        data['foundCycle'] = (len(data['cycles']) > 0)
+
+        self.data = data
+
+        return self
+
+    def __repr__(self):
+        return str(self.data)
+
+    def __str__(self):
+        s = "Cycles("
+        if self.data:
+            for sc, _ in enumerate(self.data['cycles']):
+                s += "\n\t{ "
+                for vert, _ in enumerate(self.data['cycles'][sc]):
+                    s += self.data['cycles'][sc][vert]['vertex']['vertexID']
+                    if self.data['cycles'][sc][vert]['refLabel'] == '#interface_ref':
+                        s += "<~implements~ "
+                    elif self.data['cycles'][sc][vert]['refLabel'] == '#union_ref':
+                        s += " -union-> "
+                    elif self.data['cycles'][sc][vert]["refLabel"]:
+                        s += " -[%s]-> " % self.data['cycles'][sc][vert]["refLabel"]
+                    else:
+                        s += " }"
+        s += "\n)"
+        return s
+
+
+class TarjanAlgorithm:
+    # XXX: It will have destructive effect on the graph object
+    def __init__(self, graph, select):
+        self.graph = graph._representation
+        self.stack = []
+        self.index = 0
+        self.found_cycle = False
+        self.scc = []
+        self.input = select
+
+    def execute(self):
+        for vertex in self.graph.keys():
+            if self.found_cycle:
+                return True
+            elif 'visited' not in self.graph[vertex]:
+                self.strong_connect(self.graph[vertex])
+
+    def strong_connect(self, vertex):
+
+        vertex['index'] = self.index
+        vertex['lowlink'] = self.index
+        self.index += 1
+        self.stack.append(vertex)
+        vertex['visited'] = True
+        vertex['onStack'] = True
+        for reference, _ in enumerate(vertex['referenceList']):
+
+            if 'visited' not in vertex['referenceList'][reference]['reference']:
+                self.strong_connect(vertex['referenceList'][reference]['reference'])
+                vertex['lowlink'] = min(vertex['lowlink'], vertex['referenceList'][reference]['reference']['lowlink'])
+            elif 'onStack' in vertex['referenceList'][reference]['reference'] and \
+                    vertex['referenceList'][reference]['reference']['onStack']:
+                vertex['lowlink'] = min(vertex['lowlink'], vertex['referenceList'][reference]['reference']['index'])
+                if self.input:
+                    self.found_cycle = True
+                    return
+
+        if vertex['lowlink'] == vertex['index']:
+            tmp_scc = []
+            w = self.stack.pop()
+            w['lowlink'] = vertex['index']
+            w['onStack'] = False
+            w['jIndex'] = len(tmp_scc) # XXX: Used in johnson later
+            tmp_scc.append(w)
+            while w != vertex:
+                w = self.stack.pop()
+                w['lowlink'] = vertex['index']
+                w['onStack'] = False
+                w['jIndex'] = len(tmp_scc) # XXX: Used in johnson later
+                tmp_scc.append(w)
+            self.scc.append(tmp_scc)
+
+    def prune_edges(self):
+        scc = self.scc
+        for component, _ in enumerate(scc):
+            for vertex, _ in enumerate(scc[component]):
+                tmp_reference_list = scc[component][vertex]['referenceList'][:] # copy list to operate on that
+                i = 0
+                for ref, _ in enumerate(scc[component][vertex]['referenceList']):
+                    if scc[component][vertex]['referenceList'][ref]['reference']['lowlink'] != scc[component][vertex]['lowlink']:
+                        del tmp_reference_list[ref+i]
+                        i -= 1
+                scc[component][vertex]['referenceList'] = tmp_reference_list
+
+
+class JohnsonAlgorithm:
+    def __init__(self, component):
+        self.component = component
+        self.cycles = []
+
+        self.blocked = [False for _ in self.component]
+        self.blocked_map = [[] for _ in self.component]
+        self.stack = []
+        self.stack_edges = []
+        self.found_cycle = False
+        self.start_vertex = 0
+
+    def execute(self):
+        for vertex, _ in enumerate(self.component):
+            self.start_vertex = vertex
+            self.find_cycles(vertex)
+            self.blocked = [False for _ in self.component]
+            self.blocked_map = [[] for _ in self.component]
+
+    def unblock(self, u):
+        self.blocked[u] = False
+        for w, _ in enumerate(self.blocked_map[u]):
+            target_block = self.blocked_map[u][w]
+            if self.blocked[target_block]:
+                self.unblock(target_block)
+        self.blocked_map[u] = []
+
+    def find_cycles(self, v):
+        result = []
+        self.stack.append(v) # push indexdx of vertex (in component)
+        self.blocked[v] = True
+        for edge, _ in enumerate(self.component[v]['referenceList']):
+            edge_ref = self.component[v]['referenceList'][edge]
+            if edge_ref['reference']['jIndex'] == self.start_vertex:
+                self.found_cycle = True
+                self.stack_edges.append(edge_ref['label'])
+
+                for item, _ in enumerate(self.stack):
+                    result.append({
+                        "vertex": self.component[self.stack[item]],
+                        "refLabel": self.stack_edges[item]
+                    })
+                result.append({
+                    "vertex": self.component[self.start_vertex],
+                    "refLabel": ""
+                })
+                if self.stack_edges: self.stack_edges.pop()
+                # save result
+                self.cycles.append(result)
+                result = []
+            elif not self.blocked[edge_ref['reference']['jIndex']] and self.start_vertex < edge_ref['reference']['jIndex'] \
+                and edge_ref['reference']['jIndex'] not in self.stack:
+                self.stack_edges.append(edge_ref['label'])
+                self.found_cycle = self.find_cycles(edge_ref['reference']['jIndex']) # bug ?
+
+        if self.found_cycle:
+            self.unblock(v)
+        else:
+            for wt, _ in enumerate(self.component[v]['referenceList']):
+                w = self.component[v]['referenceList'][wt]['reference']['jIndex']
+                if v not in self.blocked_map[w]:
+                    self.blocked_map[w].append(v)
+
+        if self.stack: self.stack.pop()
+        if self.stack_edges: self.stack_edges.pop()
+        return self.found_cycle
