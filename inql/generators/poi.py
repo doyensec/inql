604b9fc39a53e266b9f9847d9c7332a24e90254d
Schooby <andrew.schoo@outlook.com>
2023-01-08 14:40:02 -0600

    added poi functionality,to matrix || need to add regexes list

--- inql/generators/poi.py
+++ inql/generators/poi.py
@@ -1,42 +1,133 @@
 from inql.utils import simplify_introspection
+import re
+
 GRAPHQL_BUILTINS = ["Int", "String", "ID", "Boolean", "Float"]
+
+
 class Graph:
-    def __init__(self,graph=None,data=None):
-        self.graph = graph or {}
+    def __init__(self, nodes=None, data=None, schema=None, functions=None):
+        self.nodes = nodes or {}
+        self.schema = schema or {}
+        self.functions = functions or {}
         self.data = data or {}
-    def get(self,node_name):
-        node = self.graph.get(node_name,False)
-        if node:
+
+    def node(self, node_name=None):
+        if node_name:
+            node = self.nodes.get(node_name, Node(node_name))
+            self.nodes[node_name] = node
             return node
         else:
-            breakpoint()
-            self.data[node_name]
-            
+            return {k: v for k, v in self.nodes.items()}
+
+    def function(self, function_name=None):
+        if function_name:
+            node = self.functions.get(function_name, Node(function_name))
+            self.functions[function_name] = node
+            return node
+        else:
+            return {k: v for k, v in self.functions.items()}
+
+    def generate(self):
+        for obj_name, obj_data in self.data.items():
+            if obj_name in self.schema:
+                node = self.schema[obj_name]
+                for field_name, field_data in obj_data.items():
+                    field = self.function(field_data["type"])
+                    node.add_child(field_name, field)
+                    field.add_parent(node)
+            else:
+                node = self.node(obj_name)
+                for field_name, field_data in obj_data.items():
+                    if field_name in ["__implements"]:
+                        continue
+                    elif field_data["type"].startswith(tuple(GRAPHQL_BUILTINS)):
+                        field = field_data["type"]
+                    else:
+                        field = self.node(field_data["type"])
+                        field.add_parent(node)
+                    node.add_child(field_name, field)
+
     def __str__(self):
-        return ""
+        return f"Graph()"
+
     def __repr__(self):
-        return ""
+        return f"Graph()"
+
+    def gen_poi(self):
+        def isInteresting(s):
+            return "pass" in s
+        poi = {"functions":{},"nodes":[],"fields":{}}
+        # interesting function names
+        for node in self.schema.values():
+            print(node.name)
+            for name in node.children:
+                if isInteresting(name):
+                    arr = poi["functions"].get(node.name,[])
+                    arr.append(name)
+                    poi["functions"][node.name] = arr
+        # interesting object names
+        for name in self.nodes:
+            if isInteresting(name):
+                poi["nodes"].append(name)
+        # interesting field names
+        for node in self.nodes.values():
+            for field in node.children:
+                if isInteresting(field):
+                    arr = poi["fields"].get(node.name,[])
+                    arr.append(field)
+                    poi["fields"][node.name] = arr
+        return poi
+    
+    def gen_matrix(self):
+        keys = {name:idx for idx,name in enumerate(self.nodes)}
+        length = len(keys)
+        matrix = [[0]*length]*length
+        for node in self.nodes.values():
+            row = keys[node.name]
+            for field in node.children.values():
+                if isinstance(field,str):
+                    # must be a scalar
+                    continue
+                matrix[row][keys[field.name]] = 1
+        return matrix
+            
+
 class Node:
-    def __init__(self,name,ntype='',inputs=None,children=None,parents=None,raw=None):
+    def __init__(
+        self, name, ntype="", inputs=None, children=None, parents=None, raw=None
+    ):
         self.name = name
         self.ntype = ntype or "Object"
         self.inputs = inputs or ...
         self.children = children or {}
         self.parents = parents or {}
         self.raw = raw or {}
-    def add_child(self,field_name,child):
+
+    def add_child(self, field_name, child):
         self.children[field_name] = child
-    def add_parent(self,parent):
+
+    def add_parent(self, parent):
         self.parents[parent.name] = parent
+
     def __str__(self):
         return f"{self.ntype}(name={self.name})"
+
     def __repr__(self):
         return f"{self.ntype}(name={self.name})"
+
     def __hash__(self):
-        return hash((self.name,self.ntype))
-    def __eq__(self,other):
-        return isinstance(other,Node) and (self.name,self.ntype) == (other.name,self.ntype)
-    def __ne__(self,other):
+        return hash((self.name, self.ntype))
+
+    def __eq__(self, other):
+        return isinstance(other, Node) and (
+            (self.name, self.ntype)
+            == (
+                other.name,
+                self.ntype,
+            )
+        )
+
+    def __ne__(self, other):
         return not (self == other)
 
 
@@ -51,16 +142,15 @@ def generate(argument, fpath="cycles.txt", green_print=lambda s: print(s)):
     green_print("Generating POI's")
     # simplify schema
     si = simplify_introspection(argument)
-    schema = [Node(name=v["type"],ntype=k) for k,v in si["schema"].items()]
     # all nodes will have a name and their corresponding object
-    graph = Graph(data=si["type"])
-    # get high level function names and return types
-    for query in schema:
-        for func,data in si["type"].pop(query.name).items():
-            function_gqlo = Node(name=func,type="function",raw=data)
+    graph = Graph(
+        schema={
+            v["type"]: Node(name=v["type"], ntype=k) for k, v in si["schema"].items()
+        },
+        data=si["type"],
+    )
+    graph.generate()
 
-            
-            function_gqlo.add_child(nodes[data["type"]])
-            query.add_child(function_gqlo)
-    
+    report = graph.gen_poi()
+
     breakpoint()
