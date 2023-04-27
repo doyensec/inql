e905fb3d75db4ffe2cd09879af140a26b4b899eb
Andrew Konstantinov <105389353+execveat@users.noreply.github.com>
2023-04-28 00:39:52 +0200

    Exit gracefully if Burp version too old #103

--- kotlin/BurpExtender.kt
+++ kotlin/BurpExtender.kt
@@ -1,5 +1,8 @@
 package burp
 
+import java.io.File
+import java.io.PrintWriter
+
 import org.python.util.PythonInterpreter
 import org.python.core.PyObject
 
@@ -20,9 +23,61 @@ class BurpExtender: IBurpExtender, IExtensionStateListener, BurpExtension {
     private var jython: PythonInterpreter? = null
     private var pythonPlugin: PyObject? = null
 
-    init {
+    // Legacy API gets instantiated first
+    override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks) {
+        // InQL requires Burp version 2023.1.2 or higher (this is the first stable release of Montoya API):
+        // https://portswigger.net/burp/releases/professional-community-2023-1-2?requestededition=professional
+
+        // Unfortunately, versions below 2022.3.3 will not load an extension at all, as they aren't compatible
+        // with JVM 17. But at least for those running on JVM 17, we can show a more helpful error message if
+        // they don't support Montoya API.
+
+        // getBurpVersion() returns an array of strings, e.g. for 2022.3.3 CE: ['Burp Suite Community Edition', '2022', '3.3']
+        val version_array = callbacks.getBurpVersion()
+
+        // show helpful error message if version is below 2023.1.2 (version_array[1] is year, version_array[2] is major and minor style like '1.2')
+        // split version_array[2] before comparing
+        val year = version_array[1].toInt()
+        val major = version_array[2].split(".")[0].toInt()
+        val minor = version_array[2].split(".")[1].toInt()
+        if ((year < 2023) or ((year == 2023) and (major == 1) and (minor < 2))) {
+            val stdout = PrintWriter(callbacks.stdout, true)
+
+            stdout.println("InQL v5 depends on Montoya API which requires Burp version 2023.1.2 or higher.")
+            stdout.println("Your Burp version is older and isn't supported: ${version_array[1]}.${version_array[2]}")
+            stdout.println("")
+            stdout.println("If InQL stopped working suddenly, it probably was installed from the BApp Store and")
+            stdout.println("received an automatic update to v5.")
+            stdout.println("")
+            stdout.println("Update your Burp installation or revert to InQL v4 by installing it manually from:")
+            stdout.println("https://github.com/doyensec/inql/releases")
+
+            callbacks.unloadExtension()
+            throw Exception("Burp version is too low for InQL v5.")
+        }
+
+        // Save legacy API for the functionality that still relies on it
+        legacyApi = callbacks
+
         // Start embedded Python interpreter session (Jython)
         jython = PythonInterpreter()
+    }
+
+    // Montoya API gets instantiated second
+    override fun initialize(montoyaApi: MontoyaApi) {
+        // The new Montoya API should be used for all of the new functionality in InQL
+        montoya = montoyaApi
+
+        // Set the name of the extension
+        montoya!!.extension().setName("InQL v$version")
+        // The legacy API:
+        //callbacks.setExtensionName("InQL v$version")
+
+        // Instantiate the legacy Python plugin
+        pythonPlugin = legacyPythonPlugin()
+
+        // Pass execution to legacy Python code
+        pythonPlugin!!.invoke("registerExtenderCallbacks")
     }
 
     private fun legacyPythonPlugin(): PyObject {
@@ -41,29 +96,6 @@ class BurpExtender: IBurpExtender, IExtensionStateListener, BurpExtension {
         jython!!.exec("del callbacks, montoya")
 
         return legacyPlugin
-    }
-
-    // Legacy API gets instantiated first
-    override fun registerExtenderCallbacks(callbacks: IBurpExtenderCallbacks) {
-        // Save legacy API for the functionality that still relies on it
-        legacyApi = callbacks
-    }
-
-    // Montoya API gets instantiated second
-    override fun initialize(montoyaApi: MontoyaApi) {
-        // The new Montoya API should be used for all of the new functionality in InQL
-        montoya = montoyaApi
-
-        // Set the name of the extension
-        montoya!!.extension().setName("InQL v$version")
-        // The legacy API:
-        //callbacks.setExtensionName("InQL v$version")
-
-        // Instantiate the legacy Python plugin
-        pythonPlugin = legacyPythonPlugin()
-
-        // Pass execution to legacy Python code
-        pythonPlugin!!.invoke("registerExtenderCallbacks")
     }
 
     override fun extensionUnloaded() {
