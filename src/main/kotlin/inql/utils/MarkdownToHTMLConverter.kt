8a677068aaf7d7e2c3d4b193d2507aa633316dd2
Bartek Gorkiewicz <bartek@doyensec.com>
2025-11-06 13:45:01 +0100

    Removing okhttp from dependencies

--- src/main/kotlin/inql/utils/MarkdownToHTMLConverter.kt
+++ src/main/kotlin/inql/utils/MarkdownToHTMLConverter.kt
@@ -1,18 +1,32 @@
 package inql.utils
 
+import burp.Burp
+import burp.api.montoya.http.HttpService
 import com.vladsch.flexmark.parser.Parser
 import com.vladsch.flexmark.html.HtmlRenderer
-import okhttp3.OkHttpClient
-import okhttp3.Request
+import burp.api.montoya.http.message.requests.HttpRequest
+import java.net.URL
 
 internal object MarkdownToHtmlConverter {
     fun downloadMarkdown(url: String): String {
-        val client = OkHttpClient()
-        val request = Request.Builder().url(url).build()
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw Exception("Failed to fetch ${url}: ${response.code}")
-            return response.body?.string() ?: throw Exception("Empty response body")
-        }
+        val parsedUrl = URL(url)
+        var path = parsedUrl.path.ifEmpty { "/" }
+        val host = parsedUrl.host
+        if (parsedUrl.query?.isNotBlank() == true) path = "$path?${parsedUrl.query}"
+        val request: HttpRequest = HttpRequest.httpRequest()
+            .withService(HttpService.httpService(url))
+            .withMethod("GET")
+            .withAddedHeader("Host", host)
+            .withPath(path)
+            .withBody("")
+            .withDefaultHeaders()
+
+        val response = Burp.Montoya.http().sendRequest(request)
+        val resp = response.response() ?: throw Exception("Failed to fetch $url")
+
+        val status = resp.statusCode()
+        if (status < 200 || status >= 300) throw Exception("Failed to fetch ${url}: $status")
+        return resp.body().bytes.toString(Charsets.UTF_8) // not .bodyToString() because we want to keep emojis from HTML
     }
 
     fun renderMarkdownToHtml(text: String): String {
